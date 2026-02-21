50e00f79164622c60bcf443b71bd923936f78447
MTGVim <takun617@gmail.com>
2026-02-21 15:34:09 +0900

    chore(logs): received message 로그 출력 제거

--- index.js
+++ index.js
@@ -700,13 +700,6 @@ client.on("messageCreate", async (msg) => {
 
   if (!botChannel) botChannel = msg.channel;
 
-  console.log(
-    "[command] received message:",
-    content,
-    "| from:",
-    `${msg.author.username}#${msg.author.discriminator}`
-  );
-
   if (content === "!도움") {
     msg.reply(getAvailableCommandsMessage());
   }
